3aea78840e14fa9e34d403f34e4453dd37e82d71
rjruiz <rjruizsf@gmail.com>
2024-01-28 09:49:31 -0300

    Creando un API resource para categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoriaCollection;
 use App\Models\Categoria;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,10 @@ class CategoriaController extends Controller
     public function index() {
         // dd('Desde API CategoriaController');
 
-        // respuesta json
-        // nombre del modelo "Categoria" 
-        return response()->json(['categorias' => Categoria::all()]);
+        // 1er tipo de respuesta json        
+        // return response()->json(['categorias' => Categoria::all()]); // nombre del modelo "Categoria"  
+        
+        // 2do tipo de respuesta json
+        return new CategoriaCollection(Categoria::all());
     }   
 }
