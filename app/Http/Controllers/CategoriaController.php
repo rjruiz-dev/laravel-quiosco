7aab5114cd37d25872acb3abbe2c8028a45e9048
rjruiz <rjruizsf@gmail.com>
2024-01-28 09:37:43 -0300

    Creando una respuesta JSON para las categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index() {
+        // dd('Desde API CategoriaController');
+
+        // respuesta json
+        // nombre del modelo "Categoria" 
+        return response()->json(['categorias' => Categoria::all()]);
+    }   
 }
